d764a22602c2ac7752ff85ff6cccb54a0b3ffef6
superjugy <superjugy@gmail.com>
2014-08-28 21:33:51 -0500

    Add code to show the return code from gradle.

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -2,25 +2,28 @@
 
 echo "Running plugin tests."
 ./gradlew clean check install
-if [ "$?" -ne 0 ]
+temp="$?"
+if [ "$temp" -ne 0 ]
 then
- echo "Error during the plugin tests."
+ echo "Error during the plugin tests. Gradle returned $temp."
  exit 1
 fi
 echo "Success in running the plugin tests. Now running the plugin in a dummy application project."
 cd example
 ../gradlew clean test
-if [ "$?" -ne 0 ]
+temp="$?"
+if [ "$temp" -ne 0 ]
 then
- echo "Error during the dummy application project tests."
+ echo "Error during the dummy application project tests. Gradle returned $temp."
  exit 1
 fi
 echo "Success in running the dummy application tests. Now running the plugin in a dummy library project."
 cd ../example-library
 ../gradlew clean test
-if [ "$?" -ne 0 ]
+temp="$?"
+if [ "$temp" -ne 0 ]
 then
- echo "Error during the dummy library project tests."
+ echo "Error during the dummy library project tests. Gradle returned $temp."
  exit 1
 fi
 echo "Plugin tested succesfully"
